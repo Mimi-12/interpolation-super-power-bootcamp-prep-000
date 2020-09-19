7a3e66a3d3b72ef2a065105c9054295de9dce642
selam lemma <oneiconforlife26@gmail.com>
2020-09-19 14:25:20 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
-colors = []
 def display_rainbow(colors)
   puts "R: #{red[0]}, O: #{orange[1]}, Y: #{yellow[2]}, G: #{green[3]}, B: #{blue]4}, I: #{indigo[5]}, V: #{violet[6]}"
 end
+print display_rainbow(colors)
